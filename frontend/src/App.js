e19fa9686b790be124498c621c7a002f7c49a16a
Akhourshed <ahmed.khourshed@student.guc.edu.eg>
2023-10-08 23:31:32 +0300

    updated apps

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,28 +1,25 @@
-import logo from './logo.svg';
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-
 import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
-import GuestHomePage from './pages/Guest/GuestHomePage';
-import GuestBox from './components/Guest/GuestBox';
+import GuestHomePage from "./pages/Guest/GuestHomePage";
+//import Login from "./pages/Guest/Login";
 
 function App() {
   return (
-    <div className= "bg-light">
-      {/* <h1>HI</h1>
-      <Routes>
-        <Route path="/" element={<GuestHomePage />} />
-       </Routes>  */}
-      <Routes>
-        <Route path="/" element={<GuestHomePage />} />
-    
-        <Route path="GuestHomePage">
-          <Route index element={<GuestHomePage />} />
-        </Route>
-
-        <Route path="*" element={<>Page not found</>} />
-      </Routes> 
-    </div>
+    <BrowserRouter>
+      <div className="bg-light">
+        <Routes>
+          <Route path="/" element={<GuestHomePage />} />
+          <Route path="GuestHomePage">
+            <Route index element={<GuestHomePage />} />
+          </Route>
+        {/*   <Route path="login">
+            <Route index element={<Login />} />
+          </Route> */}
+          <Route path="*" element={<>Page not found</>} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
