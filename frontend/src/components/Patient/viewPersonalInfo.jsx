14e91f24570863639a19e820878f2c9a6b0fa2c2
abdullahhanyy <abdullahhany02@gmail.com>
2023-11-11 17:34:21 +0300

    editing myprofile

--- frontend/src/components/Patient/viewPersonalInfo.jsx
+++ frontend/src/components/Patient/viewPersonalInfo.jsx
@@ -1,17 +1,17 @@
 import React, { useState } from "react";
-import { ListGroup } from "react-bootstrap";
-/* import { useSelector } from "react-redux"; */
+import { ListGroup, Button } from "react-bootstrap";
 import ChangePass from "../../forms/ChangePass";
 
 function ViewPersonalInfo() {
-/*   const user = useSelector((state) => state.patientLogin); */
   const [showChangePass, setShowChangePass] = useState(false);
+  const [showAddAddressInput, setShowAddAddressInput] = useState(false);
+  const [newAddress, setNewAddress] = useState(""); // Store the input value
 
   const listItemStyle = {
-    fontSize: "1rem", // Font size for all information
-    marginBottom: "0.7rem", // Margin bottom for all information
-    verticalAlign: "top", // Align items at the top of each column
-    fontWeight: "600", // Bold font weight for information title
+    fontSize: "1rem",
+    marginBottom: "0.7rem",
+    verticalAlign: "top",
+    fontWeight: "600",
   };
 
   const labelStyle = {
@@ -25,12 +25,29 @@ function ViewPersonalInfo() {
     setShowChangePass(!showChangePass);
   };
 
+  const handleAddNewAddressClick = () => {
+    setShowAddAddressInput(true);
+  };
+
+  const handleNewAddressInputChange = (e) => {
+    setNewAddress(e.target.value);
+  };
+
+  const handleSaveNewAddress = () => {
+    // Perform the logic to save the new address
+    console.log("New address:", newAddress);
+
+    // Reset the state and hide the input field
+    setNewAddress("");
+    setShowAddAddressInput(false);
+  };
+
   return (
     <div>
       <div
         className="d-flex justify-content-center align-items-center"
         style={{
-          fontSize: "2.5rem", // Increase font size for the title
+          fontSize: "2.5rem",
           fontWeight: "600",
           color: "#212529",
           lineHeight: "1.5",
@@ -41,17 +58,19 @@ function ViewPersonalInfo() {
       <ListGroup>
         <ListGroup.Item>
           <div style={listItemStyle}>
-            <span style={{ color: "#099BA0" }}>Name:</span> {/* {user.name} */}
+            <span style={{ color: "#099BA0" }}>Name:</span>
+            {/* Display user's name */}
           </div>
           <div style={listItemStyle}>
-            <span style={{ color: "#099BA0" }}>Username:</span> {/* {user.userName} */}
+            <span style={{ color: "#099BA0" }}>Username:</span>
+            {/* Display user's username */}
           </div>
           <div style={listItemStyle}>
-            <span style={{ color: "#099BA0" }}>Password:</span>{" "}
+            <span style={{ color: "#099BA0" }}>Password:</span>
             <span>
               <label
                 style={labelStyle}
-                onClick={toggleChangePass} // Add your click handler here
+                onClick={toggleChangePass}
               >
                 {showChangePass ? "close" : "change password"}
               </label>
@@ -61,12 +80,51 @@ function ViewPersonalInfo() {
             )}
           </div>
           <div style={listItemStyle}>
-            <span style={{ color: "#099BA0" }}>Email:</span> {/* {user.userEmail} */}
+            <span style={{ color: "#099BA0" }}>Email:</span>
+            {/* Display user's email */}
           </div>
           <div style={listItemStyle}>
-            <span style={{ color: "#099BA0" }}>Phone Number:</span>{" "}
-            {/* {user.phoneNumber} */}
+            <span style={{ color: "#099BA0" }}>Phone Number:</span>
+            {/* Display user's phone number */}
           </div>
+          <div style={listItemStyle}>
+            <span style={{ color: "#099BA0" }}>My Addresses:</span>
+          </div>
+          <div style={{ marginLeft: "2rem" }}>
+            <span>Address 1:</span>
+            {/* Display existing address 1 */}
+          </div>
+          <div style={{ marginLeft: "2rem" }}>
+            <span>Address 2:</span>
+            {/* Display existing address 2 */}
+          </div>
+          <div style={{ marginLeft: "2rem" }}>
+            <span>Address 3:</span>
+            {/* Display existing address 3 */}
+          </div>
+          {showAddAddressInput ? (
+            <div>
+              <input
+                type="text"
+                placeholder="Enter new address"
+                value={newAddress}
+                onChange={handleNewAddressInputChange}
+              />
+              <Button
+                variant="primary"
+                onClick={handleSaveNewAddress}
+              >
+                Save
+              </Button>
+            </div>
+          ) : (
+            <Button
+              variant="primary"
+              onClick={handleAddNewAddressClick}
+            >
+              Add New Address
+            </Button>
+          )}
         </ListGroup.Item>
       </ListGroup>
     </div>
