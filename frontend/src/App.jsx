087b5c1076ff80c525470f0dde525f5679dbcb7b
mariamelerian <mariamelerian@gmail.com>
2023-12-14 19:45:47 +0200

    removed chat withpharm eladema

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -32,10 +32,15 @@ import ResetPassword from "./pages/Guest/ResetPassword";
 import PasswordChanged from "./pages/Guest/PasswordChanged";
 import CheckOutDoneModal from "./components/Patient/PatientCheckOutDoneModal";
 import PatientShowMedicine from "./components/Patient/PatientShowMedicine";
+<<<<<<< Updated upstream
 import PatientChatWithPharmacist from "./pages/Patient/PatientChatWithPharmacist";
 //import ChatPat from "./components/ChatPat";
 import AdminSalesReportPage from "./pages/Admin/AdminSalesReportPage";
 import PharmacistSalesReportPage from "./pages/Pharmacist/PharmacistSalesReportPage";
+=======
+import ChatPat from "./components/ChatPat";
+  
+>>>>>>> Stashed changes
 
 //import AddMedicineModal from "./components/Pharmacist/PhNewMedicineModal";
 
@@ -94,6 +99,7 @@ function App() {
 
           <Route path="patient-cartItem" element={<CartItems />} />
           {/* <Route path="patient-checkout" element={<PatientCheckOutModal />} /> */}
+<<<<<<< Updated upstream
           <Route
             path="patient-checkoutcomplete"
             element={<CheckOutDoneModal />}
@@ -102,6 +108,13 @@ function App() {
             path="patient-chatwithpharmacist"
             element={<PatientChatWithPharmacist />}
           />
+=======
+          <Route path="patient-checkoutcomplete" element={<CheckOutDoneModal />} />
+          {/* <Route path="patient-chatwithpharmacist" element={<PatientChatWithPharmacist />} /> */}
+          <Route path="patient-chatwithpharmacist" element={<ChatPat />} />
+          
+          
+>>>>>>> Stashed changes
 
           <Route path="patient-myOrders" element={<PatientMyOrdersPage />} />
           <Route path="profile" element={<PatientProfile />} />
