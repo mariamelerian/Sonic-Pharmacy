61e18793373322ace15938a474e9adb2f6327690
Aya <67323302+aya-abdelaal@users.noreply.github.com>
2023-12-16 01:05:28 +0200

    prescribed medicine

--- frontend/src/components/Patient/PatientShowPrescribedMedicine.jsx
+++ frontend/src/components/Patient/PatientShowPrescribedMedicine.jsx
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from "react";
-import { Card, Col, Row, Form, Spinner, Tabs, Tab  } from "react-bootstrap";
+import { Card, Col, Row, Form, Spinner, Tabs, Tab } from "react-bootstrap";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faInfoCircle ,faSearch} from "@fortawesome/free-solid-svg-icons";
-import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
+import { faInfoCircle, faSearch } from "@fortawesome/free-solid-svg-icons";
+import { faShoppingCart } from "@fortawesome/free-solid-svg-icons";
 import { deleteFilterArray } from "../../state/filterMedicine";
 import { useSelector, useDispatch } from "react-redux";
 import axios from "axios";
@@ -38,7 +38,7 @@ function PatientPrescribedMedicine() {
 
   const fetchData = async () => {
     try {
-      const response = await axios.get("/medicines");
+      const response = await axios.get("/prescribedMedicines");
       if (response.status === 200) {
         setResponseData(response.data);
         setLoading(false);
@@ -126,138 +126,192 @@ function PatientPrescribedMedicine() {
       ) : (
         <Row>
           {medicines.map((medicine, index) => (
-  <Col key={medicine.medicineName} lg={4} md={4} sm={12}>
-    <Card className="mb-4 mx-3 bg-light">
-      <Card.Body className="text-center">
-        <div className="medicine-container">
-         {/* Add the "out of stock" label with a box */}
-         {medicine.quantity === 0 && (<div
-      className="prescribed-label"
-      style={{
-        position: 'absolute', // Set position to absolute
-        top: '5px', // Set distance from the top
-        right: '5px', // Set distance from the right
-        paddingRight:'5px',
-        paddingLeft:'5px',
-        paddingBottom:'5px',
+            <Col key={medicine.medicineName} lg={4} md={4} sm={12}>
+              <Card className="mb-4 mx-3 bg-light">
+                <Card.Body className="text-center">
+                  <div className="medicine-container">
+                    {/* Add the "out of stock" label with a box */}
+                    {medicine.quantity === 0 && (
+                      <div
+                        className="prescribed-label"
+                        style={{
+                          position: "absolute", // Set position to absolute
+                          top: "5px", // Set distance from the top
+                          right: "5px", // Set distance from the right
+                          paddingRight: "5px",
+                          paddingLeft: "5px",
+                          paddingBottom: "5px",
 
+                          // borderColor:'lightgreen',
+                          // background: 'white', // Set background color
+                          borderRadius: "8px", // Set border-radius for curved corners
+                          padding: "8px", // Set padding for space inside the box
+                        }}
+                      >
+                        <div
+                          style={{
+                            backgroundColor: "white",
+                            color: "red",
+                            border: "1px solid red",
+                            borderRadius: "5px",
+                            padding: "5px",
+                          }}
+                        >
+                          Out of Stock
+                        </div>
+                      </div>
+                    )}
 
-        // borderColor:'lightgreen',
-        // background: 'white', // Set background color
-        borderRadius: '8px', // Set border-radius for curved corners
-        padding: '8px', // Set padding for space inside the box
-      }}
-    >
-            <div style={{ backgroundColor: 'white', color: 'red', border: '1px solid red', borderRadius: '5px', padding:'5px' }}>
-        
-        Out of Stock 
-      </div>
+                    <div className="medicine-image-container">
+                      <img
+                        src={medicine.picture}
+                        alt={medicine.name}
+                        style={medicineImage}
+                      />
+                    </div>
+                    <div className="details-container">
+                      <div className="d-flex justify-content-between align-items-center mb-7 px-5">
+                        <div
+                          className="medicine-name font-weight-bold"
+                          style={{
+                            fontSize: "24px",
+                            fontStyle: "normal",
+                            fontWeight: 700,
+                            lineHeight: "120%",
+                          }}
+                        >
+                          {medicine.name}
+                        </div>
+                        <div
+                          className="expand-button"
+                          onClick={() => handleExpand(index)}
+                        >
+                          <FontAwesomeIcon icon={faInfoCircle} />
+                        </div>
+                      </div>
+                      <div
+                        className="info-price-container d-flex justify-content-between align-items-center px-5"
+                        style={{
+                          color: "#777777",
+                          fontSize: "1rem",
+                          fontStyle: "normal",
+                          fontWeight: 500,
+                          lineHeight: "100%",
+                          marginBottom: "0.5rem",
+                        }}
+                      >
+                        <div className="medicine-price">
+                          Price: {medicine.price} LE
+                        </div>
+                        <div>Prescribed</div>
+                      </div>
 
-    </div>
-      )} 
+                      {expandedMedicine === index && (
+                        <>
+                          <div
+                            className="medicine-description"
+                            style={{
+                              textAlign: "left",
+                              paddingLeft: "50px",
+                              display: "flex",
+                              alignItems: "center",
+                            }}
+                          >
+                            <h6
+                              style={{
+                                marginRight: "10px",
+                                fontWeight: "bold",
+                              }}
+                            >
+                              Description:
+                            </h6>
+                            <p
+                              style={{ marginBottom: "5px", fontSize: "14px" }}
+                            >
+                              {medicine.description}
+                            </p>
+                          </div>
+                          <div
+                            className="medicine-use"
+                            style={{
+                              textAlign: "left",
+                              paddingLeft: "50px",
+                              display: "flex",
+                              alignItems: "center",
+                            }}
+                          >
+                            <h6
+                              style={{
+                                marginRight: "10px",
+                                fontWeight: "bold",
+                              }}
+                            >
+                              Medicinal Use:
+                            </h6>
+                            <p
+                              style={{ marginBottom: "5px", fontSize: "14px" }}
+                            >
+                              {medicine.medicinalUse}
+                            </p>
+                          </div>
 
-          <div className="medicine-image-container">
-            <img
-              src={medicine.picture}
-              alt={medicine.name}
-              style={medicineImage}
-            />
-          </div>
-          <div className="details-container">
-            <div className="d-flex justify-content-between align-items-center mb-7 px-5">
-              <div
-                className="medicine-name font-weight-bold"
-                style={{
-                  fontSize: "24px",
-                  fontStyle: "normal",
-                  fontWeight: 700,
-                  lineHeight: "120%",
-                }}
-              >
-                {medicine.name}
-              </div>
-              <div
-                className="expand-button"
-                onClick={() => handleExpand(index)}
-              >
-                <FontAwesomeIcon icon={faInfoCircle} />
-              </div>
-            </div>
-            <div
-              className="info-price-container d-flex justify-content-between align-items-center px-5"
-              style={{
-                color: "#777777",
-                fontSize: "1rem",
-                fontStyle: "normal",
-                fontWeight: 500,
-                lineHeight: "100%",
-                marginBottom: "0.5rem",
-              }}
-            >
-  <div className="medicine-price">Price: {medicine.price} LE</div>
- <div>Prescribed</div>
-</div>
-
-
-            {expandedMedicine === index && (
-           <>
-          <div className="medicine-description" style={{ textAlign: "left", paddingLeft: "50px", display: "flex", alignItems: "center" }}>
-  <h6 style={{ marginRight: "10px" , fontWeight: "bold" }}>Description:</h6>
-  <p style={{ marginBottom: "5px", fontSize: "14px" }}>
-    {medicine.description}
-  </p>
-</div>
-<div className="medicine-use" style={{ textAlign: "left", paddingLeft: "50px", display: "flex", alignItems: "center" }}>
-  <h6 style={{ marginRight: "10px", fontWeight: "bold" }}>Medicinal Use:</h6>
-  <p style={{ marginBottom: "5px", fontSize: "14px" }}>
-    {medicine.medicinalUse}
-  </p>
-</div>
-
-           <div className="medicine-activeIngredients" style={{ textAlign: "left", paddingLeft: "50px" }}>
-             <h6 style={{ marginRight: "10px", fontWeight: "bold" }}>Active Ingredients:</h6>
-             <ul
-               style={{
-                 listStyleType: "none",
-                 paddingLeft: 0,
-                 fontSize: "14px",
-               }}
-             >
-               {medicine.activeIngredients.map((ingredient, index) => (
-                 <li key={index} style={{ marginBottom: "5px" }}>
-                   • {ingredient}
-                 </li>
-               ))}
-             </ul>
-           </div>
-         </>
-         
-          
-           
-            )}
-           {medicine.quantity === 0 ? (
-            <div style={{ marginTop: '5px' }}>
-               <button
-            className="btn btn-primary mt-3"
-            onClick={() => handleFindAlternatives(medicine)}
-          >
-            Find Alternatives <FontAwesomeIcon icon={faSearch} />
-          </button>
-            </div>
-          ) : (
-            <button
-              className="btn btn-primary mt-3"
-              onClick={() => handleAddToCart(medicine)}
-            >
-              Add to Cart <FontAwesomeIcon icon={faShoppingCart} />
-            </button>
-          )}
-          </div>
-        </div>
-      </Card.Body>
-    </Card>
-  </Col>
-))}
+                          <div
+                            className="medicine-activeIngredients"
+                            style={{ textAlign: "left", paddingLeft: "50px" }}
+                          >
+                            <h6
+                              style={{
+                                marginRight: "10px",
+                                fontWeight: "bold",
+                              }}
+                            >
+                              Active Ingredients:
+                            </h6>
+                            <ul
+                              style={{
+                                listStyleType: "none",
+                                paddingLeft: 0,
+                                fontSize: "14px",
+                              }}
+                            >
+                              {medicine.activeIngredients.map(
+                                (ingredient, index) => (
+                                  <li
+                                    key={index}
+                                    style={{ marginBottom: "5px" }}
+                                  >
+                                    • {ingredient}
+                                  </li>
+                                )
+                              )}
+                            </ul>
+                          </div>
+                        </>
+                      )}
+                      {medicine.quantity === 0 ? (
+                        <div style={{ marginTop: "5px" }}>
+                          <button
+                            className="btn btn-primary mt-3"
+                            onClick={() => handleFindAlternatives(medicine)}
+                          >
+                            Find Alternatives{" "}
+                            <FontAwesomeIcon icon={faSearch} />
+                          </button>
+                        </div>
+                      ) : (
+                        <button
+                          className="btn btn-primary mt-3"
+                          onClick={() => handleAddToCart(medicine)}
+                        >
+                          Add to Cart <FontAwesomeIcon icon={faShoppingCart} />
+                        </button>
+                      )}
+                    </div>
+                  </div>
+                </Card.Body>
+              </Card>
+            </Col>
+          ))}
         </Row>
       )}
       <AddToCartModal
