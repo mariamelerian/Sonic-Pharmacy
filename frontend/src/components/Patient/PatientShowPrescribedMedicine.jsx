4a3ad20628cb16db8f876f991db6b353b45c7cb6
youstina23 <youstina.beshay@student.guc.edu.eg>
2023-12-16 19:08:01 +0200

    d

--- frontend/src/components/Patient/PatientShowPrescribedMedicine.jsx
+++ frontend/src/components/Patient/PatientShowPrescribedMedicine.jsx
@@ -145,10 +145,15 @@ function PatientPrescribedMedicine() {
             <span className="visually-hidden">Loading...</span>
           </Spinner>
         </div>
-       ) : error ? (
+      ) : error ? (
         <div className="error">{error}</div>
       ) : medicines.length === 0 ? ( // Check if there are no prescribed medicines
-        <PatientEmptyPrescribedMedicine />
+        <div
+          className="msg"
+          style={{ width: "20rem", marginLeft: "30rem", textAlign: "center" }}
+        >
+          No prescribes medicines
+        </div>
       ) : (
         <Row>
           {filteredMedicines.map((medicine, index) => (
@@ -398,7 +403,7 @@ function PatientPrescribedMedicine() {
                                   }}
                                 >
                                   <div className="medicine-price">
-                                    <strong>Price: </strong> ${medicine.price} 
+                                    <strong>Price: </strong> ${medicine.price}
                                   </div>
                                 </div>
                                 {expandedMedicineInModal === index && (
